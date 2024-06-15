1d3539387443fc4083bd278616ed4e607e7c2092
Fernando Maclen <fmaclen@gmail.com>
2024-06-15 17:52:56 -0400

    comment out PB auth

--- frontend/src/PocketBaseContext.js
+++ frontend/src/PocketBaseContext.js
@@ -6,16 +6,18 @@ export const PocketBaseContext = React.createContext();
 export const PocketBaseProvider = ({ children }) => {
   const pb = new PocketBase("http://127.0.0.1:8090");
 
-  React.useEffect(() => {
-    const authenticate = async () => {
-      await pb.admins.authWithPassword(
-        "hello@fernando.is",
-        "TUP!zar_twg7wbj*ruc"
-      );
-    };
-
-    authenticate();
-  }, []);
+  
+  // React.useEffect(() => {
+  //   const authenticate = async () => {
+  //     await pb.admins.authWithPassword(
+  //       "hello@fernando.is",
+  //       "TUP!zar_twg7wbj*ruc"
+  //     );
+  //   };
+    
+  //   authenticate();
+  //   console.log("authing");
+  // }, []);
 
   return (
     <PocketBaseContext.Provider value={pb}>
