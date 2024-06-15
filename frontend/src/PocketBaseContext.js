03ad7afbda0b4b8a80d3bd631e7dc40c99e059d3
Fernando Maclen <fmaclen@gmail.com>
2024-06-15 19:27:47 -0400

    render classes, tests, questions and answers

--- frontend/src/PocketBaseContext.js
+++ frontend/src/PocketBaseContext.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PocketBase from "pocketbase";
 
 export const PocketBaseContext = React.createContext();
 
-export const PocketBaseProvider = ({ children }) => {
-  const pb = new PocketBase("http://127.0.0.1:8090");
+export const POCKETBASE_BASE_URL = "http://127.0.0.1:8090";
 
-  
-  // React.useEffect(() => {
-  //   const authenticate = async () => {
-  //     await pb.admins.authWithPassword(
-  //       "hello@fernando.is",
-  //       "TUP!zar_twg7wbj*ruc"
-  //     );
-  //   };
-    
-  //   authenticate();
-  //   console.log("authing");
-  // }, []);
+export const PocketBaseProvider = ({ children }) => {
+  const pb = new PocketBase(POCKETBASE_BASE_URL);
+
+  useEffect(() => {
+    const authenticate = async () => {
+      try {
+        await pb.admins.authWithPassword("hello@fernando.is", "TUP!zar_twg7wbj*ruc");
+      } catch (_) {
+      }
+    };
+
+    authenticate();
+  }, []);
 
   return (
     <PocketBaseContext.Provider value={pb}>
