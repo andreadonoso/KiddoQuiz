13d45901915f91ee21528fce384c6ab3051197d3
Fernando Maclen <fmaclen@gmail.com>
2024-06-16 14:06:52 -0400

    add ScorePage

--- src/app/scores/ScoresPage.js
+++ src/app/scores/ScoresPage.js
@@ -1,5 +1,34 @@
-const ScoresPage = () => {
-	return <div> ScoresPage</div>;
+const ScoresPage = (data) => {
+  const questions = data.questionsAndAnswers;
+
+  return (
+    <div>
+      <h1>{data.test.title}</h1>
+      <ul>
+        {questions.map((qa, index) => {
+          const totalChoices = qa.answers.reduce((sum, answer) => sum + answer.choices, 0);
+
+          return (
+            <li key={index}>
+              <h3>{qa.question}</h3>
+              <ul>
+                {qa.answers.map((answer, index) => {
+                  const styling = answer.is_correct ? { color: "green" } : { color: "red" };
+                  const percentage = totalChoices == 0 ? 0 : ((answer.choices / totalChoices) * 100).toFixed(2);
+
+                  return (
+                    <li key={index} style={styling}>
+                      <strong>{answer.answer}</strong> — {answer.choices} ({percentage}%)
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ScoresPage;
--- src/app/scores/page.js
+++ src/app/scores/page.js
@@ -1,7 +1,34 @@
 import ScoresPage from "./ScoresPage";
 
+import { pocketBase } from "@/utils/pocketbase";
+
+async function getTest() {
+  const pb = await pocketBase();
+  const test = await pb.collection('tests').getFullList({
+    expand: "questions",
+    sort: "+created",
+  });
+  return test[0];
+}
+
+async function getQuestionsAndAnswers(test) {
+  const pb = await pocketBase();
+  const questions = test.expand.questions;
+  const questionsAndAnswers = [];
+
+  for (const question of questions) {
+    const questionAndAnswers = await pb.collection('questions').getOne(question.id, { expand: "answers" });
+    questionsAndAnswers.push({
+      question: questionAndAnswers.question,
+      answers: questionAndAnswers.expand ? questionAndAnswers.expand.answers : [],
+    });
+  }
+  return questionsAndAnswers;
+}
+
+
 export default async function Scores() {
-  return (
-    <ScoresPage />
-  );
+  const test = await getTest();
+  const questionsAndAnswers = await getQuestionsAndAnswers(test);
+  return <ScoresPage test={test} questionsAndAnswers={questionsAndAnswers} />;
 }
