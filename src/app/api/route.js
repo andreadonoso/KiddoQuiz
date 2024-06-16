e407fb33f33717c511f4eb8720051c7dcb91b83a
Fernando Maclen <fmaclen@gmail.com>
2024-06-16 13:03:28 -0400

    submit test and redirect to completed

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,30 +1,25 @@
-// pages/api/update-test.js
+// pages/api/route.js
 import { pocketBase } from "@/utils/pocketbase"; // Adjust the path as needed
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
     const pb = await pocketBase();
-
-    // body.req is a readable stream, so we need to parse it
-
-    console.log("req.body", req.body);
-    // req.body ReadableStream { locked: false, state: 'readable', supportsBYOB: false }
-    const body = JSON.parse(req.body);
-    console.log("body", body);
-
-    console.log("answerData", answerData);
+    const buffers = [];
+    for await (const chunk of req.body) {
+      buffers.push(chunk);
+    }
+    const data = Buffer.concat(buffers).toString('utf8');
+    const answerData = JSON.parse(data);
 
     // Update the record in PocketBase	const answers = answerData.answers;
-    // for (const answerId of answers) {
-    //   console.log(`updating answer: ${answerId}`);
-    //   const updatedAnswer = await pb.collection("answers").update(answerId, {
-    //     "choices+": 1,
-    //   });
-    //   console.log(updatedAnswer);
-    // }
-
-    // Send a success response
+    for (const answerId of answerData.answers) {
+      console.log(`updating answer: ${answerId}`);
+      const updatedAnswer = await pb.collection("answers").update(answerId, {
+        "choices+": 1,
+      });
+      console.log(updatedAnswer);
+    }
     return NextResponse.json("GOOD");
   } catch (error) {
     console.error(error)
