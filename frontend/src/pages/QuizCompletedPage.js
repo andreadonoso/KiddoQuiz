7fcb3de2ad5a245f51c607795c76e85c265c4030
Andrea Donoso <donosoandrea@icloud.com>
2024-06-16 00:23:56 -0400

    Quiz Completed Page

--- frontend/src/pages/QuizCompletedPage.js
+++ frontend/src/pages/QuizCompletedPage.js
@@ -1,5 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import mascot from "../assets/mascot.png";
+
 const QuizCompletedPage = () => {
-	return <div>QuizCompletedPage</div>;
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+				backgroundColor: "#A8E3B7",
+				width: "100%",
+				height: "100vh",
+			}}
+		>
+			<Typography variant="h3" p={5}>
+				Quiz Completed!
+			</Typography>
+			<img src={mascot} alt="Mascot" style={{ height: "50%" }} />
+		</Box>
+	);
 };
 
 export default QuizCompletedPage;
